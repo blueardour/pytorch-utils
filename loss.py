dc844ca43f7620f8b31a6a1803c13dff45f2d928
chenp@ws6 <chenp@ws6>
2021-04-30 10:14:18 +0930

    Add KD loss support

--- loss.py
+++ loss.py
@@ -1,8 +1,9 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-#label smooth
+# label smooth
 class CrossEntropyLabelSmooth(nn.Module):
   def __init__(self, num_classes, epsilon):
     super(CrossEntropyLabelSmooth, self).__init__()
@@ -16,3 +17,22 @@ class CrossEntropyLabelSmooth(nn.Module):
     targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
     loss = (-targets * log_probs).mean(0).sum()
     return loss
+
+
+# KD
+# https://github.com/peterliht/knowledge-distillation-pytorch/blob/ef06124d67a98abcb3a5bc9c81f7d0f1f016a7ef/model/net.py#L100
+def loss_fn_kd(outputs, labels, teacher_outputs, params):
+    """
+    Compute the knowledge-distillation (KD) loss given outputs, labels.
+    "Hyperparameters": temperature and alpha
+    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
+    and student expects the input tensor to be log probabilities! See Issue #2
+    """
+    alpha = params.distill_loss_alpha
+    T = params.distill_loss_temperature
+    KD_loss = nn.KLDivLoss()(F.log_softmax(outputs/T, dim=1),
+                             F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T) + \
+              F.cross_entropy(outputs, labels) * (1. - alpha)
+
+    return KD_loss
+
